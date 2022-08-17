73b27a836f14d6b1e6e3c43d63a9e62fe6b2ca53
csklzj <csklzj@csklzj.com>
2022-08-17 17:17:27 +0800

    test

--- src/main/java/cn/csklzj/Test.java
+++ src/main/java/cn/csklzj/Test.java
@@ -15,5 +15,6 @@ public class Test {
         System.out.println("push test3");
         System.out.println("push test4");
         System.out.println("push test5");
+        System.out.println("push test6");
     }
 }
