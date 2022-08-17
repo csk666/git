f97e6b833b69c9b5b91c06c0908e62b434953872
扶苏 <62277551+csklia@users.noreply.github.com>
2022-08-17 17:11:14 +0800

    push

--- src/main/java/cn/csklzj/Test.java
+++ src/main/java/cn/csklzj/Test.java
@@ -14,6 +14,5 @@ public class Test {
         System.out.println("push test3");
         System.out.println("push test3");
         System.out.println("push test4");
-        System.out.println("push test4");
     }
 }
